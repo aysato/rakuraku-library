b45ff6036cbeeef3ad283167c074f77f56c3671f
aysato <aysato@kcscorp.jp>
2017-03-31 08:46:07 +0900

    Delete style.js

--- style.js
+++
@@ -1,7 +0,0 @@
- jQuery(function(){ 
-		  $("li.librarycat").children("ul").show();
-		  $("ul.children").hide();
-		  $("li.current-cat").show();
-		  $("li.current-cat").children("ul.children").show();
-		  $("li.current-cat").parents().show();	
-});
